0e3d2e2141299be4cc9a76633197f6a870300a49
Ram Manohar <ramnew2006@gmail.com>
2013-07-03 23:12:40 +0530

    updated storetrainlist code to capture all the urls in a single run

--- storetrainlist.php
+++ storetrainlist.php
@@ -6,21 +6,23 @@ require_once 'database.php';
 $db = new database();
 $db->dbconnect();
 
-$url = "http://www.indianrail.gov.in/tourist_trn_list.html";
+$url = array("http://www.indianrail.gov.in/tourist_trn_list.html", "http://www.indianrail.gov.in/duronto_trn_list.html", "http://www.indianrail.gov.in/garibrath_trn_list.html", "http://www.indianrail.gov.in/jan_shatabdi.html", "http://www.indianrail.gov.in/shatabdi_trn_list.html", "http://www.indianrail.gov.in/rajdhani_trn_list.html", "http://www.indianrail.gov.in/mail_express_trn_list.html", "http://www.indianrail.gov.in/special_trn_list.html");
 $tablenum = 5;
 
-$train = new getcurl($url,$tablenum);
-$totalRows = $train->tableRows();
+for($k=0;$k<sizeof($url);$k++){
+	$train = new getcurl($url[$k],$tablenum);
+	$totalRows = $train->tableRows();
 
-for($i=1;$i<$totalRows;$i++){
-	$query = "INSERT INTO trainlist (train_num,train_name,source_stn,source_dep_time,dest_stn,dest_arr_time) VALUES (";
-	for($j=0;$j<6;$j++){
-		$query .= "'" . trim($train->getInfoFromRow($i,$j)) . "',";
+	for($i=1;$i<$totalRows;$i++){
+		$query = "INSERT INTO trainlist (train_num,train_name,source_stn,source_dep_time,dest_stn,dest_arr_time) VALUES (";
+		for($j=0;$j<6;$j++){
+			$query .= "'" . trim($train->getInfoFromRow($i,$j)) . "',";
+		}
+		$query = trim($query,",");
+		$query .= ")";
+		mysql_query($query);
+		//echo $query . "<br/>";
 	}
-	$query = trim($query,",");
-	$query .= ")";
-	mysql_query($query);
-	//echo $query . "<br/>";
 }
 
 $db->dbdisconnect();
